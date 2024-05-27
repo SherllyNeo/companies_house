9fa8ccc1d62696cf6ff52641dce88d1dffc5f6a4
sherllyneo <jacobp.willo@gmail.com>
2024-05-27 16:10:29 +0100

    made readable

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,47 @@ use anyhow::{Result,Error,Context};
 use reqwest::blocking::Client;
 use std::io::stdin;
 use serde_json::Value;
-use base64::encode;
+use base64::prelude::*;
 
 fn get_user_input() -> Result<String> {
+    // helper function to take user input and pop it into the buffer
     let mut buffer = String::new();
     println!("Please enter a name: ");
     stdin().read_line(&mut buffer).context("unable to read user input")?;
+
+    // this returns either a string, or an error
     Ok(buffer.trim().to_owned())
 }
 
 fn get_reqest(url: &str, api_key: &str) -> Result<Value> {
+    // helper function to get stuff
     
     // companies house is weird for this one boys
     let username = api_key;
     let password = "";
 
-    // most APIs don't use this anymore in favour of more secure methods
-    let credentials = format!("{username}:{password}");
-    let encoded_credentials = format!("basic {}",encode(credentials));
+    // most APIs don't use this anymore in favour of more secure methods. 
+    // You usually do a get request with a .header("authorization",api_key) to a special url to get
+    // a temporary token, then use that token for future requests until it times out
 
+    // for this weird one we base64 encode and use that to make an api key ish thing
+    let credentials = format!("{username}:{password}");
+    let encoded_credentials = format!("basic {}",BASE64_STANDARD.encode(credentials));
+
+    // renaming so it looks more like 'normal' 
+    let api_key_proper = encoded_credentials;
+
+    // make a new client, with a get request, add a header and send the request. usually you'd want
+    // to reuse a client over and over for speed
     let response = Client::new()
         .get(url)
-        .header("authorization",encoded_credentials)
+        .header("authorization",api_key_proper)
         .send()?;
+
+    // store the response
     let status = response.status();
+
+    // check if it worked or not
     if status.is_success() {
         // get response as json
         let response_json: Value = response
@@ -41,7 +58,7 @@ fn get_reqest(url: &str, api_key: &str) -> Result<Value> {
         Ok(top_hit.to_owned())
     }
     else {
-        // return an error
+        // if failed, return an error
         let error_text = format!("unable to get data, status {}",status);
         Err(Error::msg(error_text))
     }
@@ -78,7 +95,8 @@ fn main() -> Result<()> {
     let pretty_data = serde_json::to_string_pretty(&company_data)?;
 
 
+    // print out stuff
     println!("you typed \"{input}\" ");
-    println!("It returned \"{pretty_data}\" ");
+    println!("It returned \n\"{pretty_data}\" ");
     Ok(())
 }
